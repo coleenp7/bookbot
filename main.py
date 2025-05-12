1fb8cbab37c6b95552a2179253320aca0e60d48a
coleenp7 <coleenp7@gmail.com>
2025-05-12 10:48:02 -0600

    updated per additions to the lessons, including removing hardcoding book

--- main.py
+++ main.py
@@ -1,47 +1,35 @@
+from stats import *
+from sys import argv, exit
+import os
+
 def main():
-    with open("books/frankenstein.txt") as f:
+    if len(argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        exit(1)  # Exit with error code 1
+    
+    book_path = argv[1]
+
+    if not os.path.exists(book_path):
+        print(f"Error: The file '{book_path}' does not exist.")
+        exit(1) # Exit with error code 1
+
+    with open(book_path) as f:
         file_contents = f.read()
     # print(file_contents)
 
-    def count_words(file):
-        words = file.split()
-        return len(words)
-    # print (count_words(file_contents))
-
-    def count_characters(contents):
-        lowered_contents = contents.lower()
-        dict = {}
-        for character in lowered_contents:
-            if character in dict:
-                dict[character] += 1
-            else:
-                dict[character] = 1
-        return dict            
-    # print (count_characters(file_contents))
-
-    def sort_on(d):
-        return d["num"]
-
-    def look_pretty(dictionary):
-        sorted = []
-        for key in dictionary:
-            sorted.append({"char": key, "num": dictionary[key]})
-        sorted.sort(reverse=True, key=sort_on)
-        return sorted
-    
     word_count = count_words(file_contents)
     characters = count_characters(file_contents)
     sorted_list = look_pretty(characters)
 
-    print("--Book Report--")
-    print(f"{word_count} words found in the text")
-    print()
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
     for item in sorted_list:
          if not item["char"].isalpha():
              continue
-         print(f"The '{item['char']}' character was found {item['num']} times")
-
-    print()
-    print("--End of Report--")
+         print(f'{item['char']}: {item['num']}')
+    print("============= END ===============")
 
 main()
